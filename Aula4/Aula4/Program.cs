57228b18276f5ea2461c8b1512bec9041963f76a
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-29 19:09:13 -0300

    Aula 4 de verdade

---
+++ Aula4/Aula4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+int minutos;
+double conta;
+
+minutos = int.Parse(Console.ReadLine());
+
+conta = 50.0;
+
+if (minutos > 100)
+{
+    //conta = conta + (minutos - 100) * 2.0;
+    conta += (minutos - 100) * 2.0; // Mesmo processo - atribuição cumulativa
+
+    /*
+       a += b (a = a + b)
+       a -= b (a = a - b)
+       a /= b (a = a / b)
+       a *= b (a = a * b)
+       a %= b (a = a % b)
+
+    */
+
+}
+
+Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+Console.ReadLine();
