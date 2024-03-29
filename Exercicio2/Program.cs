77ff0d40929816ec610431e4ee998050a83693c5
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-29 18:31:42 -0300

    exercicio 2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+double A, B, C;
+
+string[] v = Console.ReadLine().Split(' ');
+
+A = double.Parse(v[0]);
+B = double.Parse(v[1]);
+C = double.Parse(v[2]);
+
+if (A <= B) {
+    Console.WriteLine("MENOR = " + A);
+}
+else if (B <= C) {
+    Console.WriteLine("MENOR = "+ B);
+}
+
+else if (C <= A) {
+    Console.WriteLine("Menor = " + C);
+}
+Console.ReadLine();
