1d4e3338114a39974d0e9c7779f3812b4ded1d37
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-29 19:43:57 -0300

    SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+/*
+    Switch-Case
+    
+    Ex:
+
+        switch(instrução){
+            case valor1:
+                comando1;
+                break;
+            case valor2:
+                comando2;
+                break;
+            case valor2:
+                comando3;
+                break;
+            deafult:
+                comando4
+                break;
+
+            
+}
+*/
+
+
+
