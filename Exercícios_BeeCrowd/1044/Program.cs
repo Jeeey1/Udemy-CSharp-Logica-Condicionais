d03c4bec580503b08635a16ba1710aea0901ab80
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-30 17:57:36 -0300

    beecrowd 1044

---
+++ Exercícios_BeeCrowd/1044/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] v = Console.ReadLine().Split(' ');
+        int A = int.Parse(v[0]);
+        int B = int.Parse(v[1]);
+
+        if (B % A == 0 || A % B == 0){
+            Console.WriteLine("Sao Multiplos");
+        }
+        else{
+            Console.WriteLine("Nao sao Multiplos");
+        }
+
+    }
+
+}
+
+/*
+
+    dif = B / A; // Se for multiplos = resultado inteiro
+
+    if (dif * A == B){ // Multiplicando resultado dif, se for multiplo irá resultar na variavel B, se der numero decimal a multiplicação não resultará na variável B
+
+        Console.WriteLine("Sao Multiplos");
+    }
+    else {
+        Console.WriteLine("Nao sao Multiplos");
+    }
+    MINHA RESOLUÇÃO PRIMÁRIA
+
+*/
