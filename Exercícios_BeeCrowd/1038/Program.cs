eeb3cf66be5ba94223c438dc3858516e6eef404b
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-30 17:36:35 -0300

    beecrowd 1038

---
+++ Exercícios_BeeCrowd/1038/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) {
+
+    string[] v = Console.ReadLine().Split(' ');
+    
+    int codigo = int.Parse(v[0]);
+    int qtd = int.Parse(v[1]);
+
+    double total;
+
+    switch (codigo){
+        case 1:
+        total = 4.0 * qtd;
+        break;
+        case 2:
+        total = 4.5 * qtd;
+        break;
+        case 3:
+        total = 5.0 * qtd;
+        break;
+        case 4:
+        total = 2.0 * qtd;
+        break;
+        default:
+        total = 1.5 * qtd;
+        break;
+    }
+    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+}
