9cd58a73a261ed8a857608326cedf0c858ddd447
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-31 21:24:00 -0300

    1036 resolvido

---
+++ Exercícios_BeeCrowd/1036/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       // BEECROWND  - ISSUE 1036 - FORMULA BHASKARA - MINHA RESOLUÇÃO
+       
+        double delta, x1, x2;
+        
+        string[] v = Console.ReadLine().Split(' ');
+
+        double A = double.Parse(v[0], CultureInfo.InvariantCulture);
+        double B = double.Parse(v[1], CultureInfo.InvariantCulture);
+        double C = double.Parse(v[2], CultureInfo.InvariantCulture);
+
+        if (A == 0){
+            Console.WriteLine("Impossivel calcular");
+        }    
+        else if (A != 0.0){
+            delta = Math.Pow(B, 2) - (4 * A * C);
+            if (delta == 0 || delta < 0){
+                Console.WriteLine("Impossivel calcular");
+            }
+            else {
+                x1 = ((-B) + Math.Sqrt(delta)) / (2.0 * A);
+                x2 = ((-B) - Math.Sqrt(delta)) / (2.0 * A);
+                Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+            
+    }
+}
--- Exercícios_BeeCrowd/1036/obj/Debug/net8.0/1036.AssemblyInfo.cs
+++ Exercícios_BeeCrowd/1036/obj/Debug/net8.0/1036.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("1044")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("1036")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03c4bec580503b08635a16ba1710aea0901ab80")]
-[assembly: System.Reflection.AssemblyProductAttribute("1044")]
-[assembly: System.Reflection.AssemblyTitleAttribute("1044")]
+[assembly: System.Reflection.AssemblyProductAttribute("1036")]
+[assembly: System.Reflection.AssemblyTitleAttribute("1036")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
