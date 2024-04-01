6634cec0bda5c84f277cdc6792120aafdc30e9ff
Joao Vitor <164676118+Jeeey1@users.noreply.github.com>
2024-03-31 22:17:10 -0300

    1046 beecrowd

---
+++ Exercícios_BeeCrowd/1046/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    string[] v = Console.ReadLine().Split(' ');
+
+    int a = int.Parse(v[0]);
+    int b = int.Parse(v[1]);
+
+    
+
+    }
+
+}
